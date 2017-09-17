9200dd581f0c23cab50e8d018a9ddb777d459bdb
David Losert <david.losert@gmx.de>
2017-09-17 16:21:59 +0200

    Extended Redux-Store to use TodoRepository conditional to the MOCK_API Variable

--- src/my-app.tsx
+++ src/my-app.tsx
@@ -2,17 +2,30 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {todo} from './state/todo.reducer';
 import TodoApp from './containers/TodoApp';
+import thunk from 'redux-thunk';
+import {createMockTodoRepository} from './todo-repository/mock/MockTodoRepository';
+import {createHTTPTodoRepository} from './todo-repository/http/HttpTodoRepository';
+import {TodoListState} from './state/TodoListState';
 
-const store = createStore(todo, {
-  loading: false,
-  todos: []
-});
+declare var MOCK_API: boolean;
+const todoRepository = MOCK_API ?
+  createMockTodoRepository()
+  : createHTTPTodoRepository('https://jsonplaceholder.typicode.com');
+console.log(`Mock-API: ${MOCK_API}`);
+const store = createStore<TodoListState>( todo, {
+	loading: false,
+	todos: []
+  },
+  applyMiddleware(
+	thunk.withExtraArgument(todoRepository)
+  )
+);
 ReactDOM.render(
   <Provider store={store}>
-	<TodoApp />
+	<TodoApp/>
   </Provider>,
   document.getElementById('root')
 );
