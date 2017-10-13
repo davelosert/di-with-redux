f426644c0de795b02c072cedb168ed0b1b1b8356
David Losert <david.losert@virtual-identity.com>
2017-10-13 17:35:48 +0200

    [chore] Made some readability adjustments

--- src/containers/TodoApp.ts
+++ src/containers/TodoApp.ts
@@ -2,17 +2,17 @@ import {connect} from 'react-redux';
 import {TodoList} from '../components/TodoList';
 import {fetchAllTodos} from '../todo-repository/fetch-todos-thunk';
 
-export function mapStateToProps({loading, todos}) {
+const mapStateToProps = ({loading, todos}) => {
   return {
     loading,
     todos
   };
-}
+};
 
-export function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
     initTodos: () => dispatch(fetchAllTodos())
   };
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todo-repository/mock/MockTodoRepository.ts
+++ src/todo-repository/mock/MockTodoRepository.ts
@@ -1,15 +1,16 @@
 import {Todo} from '../../state/Todo';
 import {TodoRepository} from '../TodoRepository';
 
-const emptyArray = Array(1000).fill(0);
-const MOCK_TODOS: Todo[] = emptyArray.map((val, index) => ({
-  id: index,
-  userId: 1,
-  title: `Todo#${index}`,
-  completed: false
-} as Todo));
 
 export const createMockTodoRepository = (): TodoRepository => ({
-  loadAllTODOs: () => Promise.resolve(MOCK_TODOS)
+  loadAllTODOs: () => Promise.resolve(createMockTodos())
 });
 
+const emptyArray = Array(1000).fill(0);
+const createMockTodos = () =>
+  emptyArray.map((val, index) => ({
+	id: index,
+	userId: 1,
+	title: `Todo#${index}`,
+	completed: false
+  } as Todo));
